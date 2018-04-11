177eb71a16e6b0a21520088427c090793ad42a0e
kelvinizer <kelvs.bernardino@gmail.com>
2018-04-11 17:18:09 +0800

    First Commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+
+<div class="sidenav">
+  <a href="#about">Home</a>
+  <a href="#services">Work</a>
+  <a href="#clients">Skills</a>
+  <a href="#contact">About Me</a>
+  <a href="#contact">Contact Me</a>
+</div>
